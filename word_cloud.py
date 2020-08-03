e9b8f7ac0824eb8f8f11f0614d2f3f2f309a2125
sumeet860 <41811254+sumeet860@users.noreply.github.com>
2020-08-03 20:25:04 +0530

    Add files via upload

---
+++ word_cloud.py
@@ -0,0 +1,58 @@
+!pip install wordcloud
+!pip install fileupload
+!pip install ipywidgets
+!jupyter nbextension install --py --user fileupload
+!jupyter nbextension enable --py fileupload
+
+import wordcloud
+import numpy as np
+from matplotlib import pyplot as plt
+from IPython.display import display
+import fileupload
+import io
+import sys
+
+# This is the uploader widget
+def upload():
+
+    upload_widget = fileupload.FileUploadWidget()
+
+    def cb(change):
+        global file_contents
+        decoded = io.StringIO(change['owner'].data.decode('utf-8'))
+        filename = change['owner'].filename
+        print('Uploaded `{}` ({:.2f} kB)'.format(
+            filename, len(decoded.read()) / 2 **10))
+        file_contents = decoded.getvalue()
+
+    upload_widget.observe(cb, names='data')
+    display(upload_widget)
+
+upload()
+
+def calculate_frequencies(file_contents):
+    # Here is a list of punctuations and uninteresting words you can use to process your text
+    punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+    uninteresting_words = ["the", "a", "to", "if", "is", "it", "of", "and", "or", "an", "as", "i", "me", "my", \
+    "we", "our", "ours", "you", "your", "yours", "he", "she", "him", "his", "her", "hers", "its", "they", "them", \
+    "their", "what", "which", "who", "whom", "this", "that", "am", "are", "was", "were", "be", "been", "being", \
+    "have", "has", "had", "do", "does", "did", "but", "at", "by", "with", "from", "here", "when", "where", "how", \
+    "all", "any", "both", "each", "few", "more", "some", "such", "no", "nor", "too", "very", "can", "will", "just"]
+    
+    # CODE START HERE
+    dic = {}
+    content = file_contents.split(" ")
+    for word in content:
+        words = " "
+        for punct in word:
+            if punct not in punctuations:
+                words = words + punct
+        if words not in uninteresting_words:
+            dic[words] = 1
+        else:
+            dic[words] += 1
+    import wordcloud
+    #wordcloud
+    cloud = wordcloud.WordCloud()
+    cloud.generate_from_frequencies(dic)
+    return cloud.to_array()
